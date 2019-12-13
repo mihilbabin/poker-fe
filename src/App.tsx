41c999dfdf13b1744a28832f794e8e19d7fc3934
Michael Babin <iamsuperman@ukr.net>
2019-12-13 03:12:29 -0500

    configure redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
 import React from "react";
+import { Provider } from "react-redux";
+
+import store from "./redux/store";
+
 import "./App.css";
 
 const App: React.FC = () => {
-  return <div></div>;
+  return (
+    <Provider store={store}>
+      <div className="App"></div>
+    </Provider>
+  );
 };
 
 export default App;
