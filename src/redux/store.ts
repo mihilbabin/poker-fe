57e7a51d474a4f50a385c93448b8ecc3db994d8c
Michael Babin <iamsuperman@ukr.net>
2020-02-17 09:50:24 +0200

    add sagas

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,4 +1,22 @@
-import { createStore } from "redux";
-import rootReducer from "./reducers";
+import { applyMiddleware, createStore } from "redux";
+import createSagaMiddleware from "redux-saga";
+import { composeWithDevTools } from "redux-devtools-extension";
 
-export default createStore(rootReducer);
+import rootReducer from "./reducers";
+import rootSaga from "./sagas";
+
+const sagaMiddleware = createSagaMiddleware();
+
+const middlewares = [sagaMiddleware];
+
+function configureStore() {
+  const store = createStore(
+    rootReducer,
+    composeWithDevTools(applyMiddleware(...middlewares))
+  );
+
+  sagaMiddleware.run(rootSaga);
+  return store;
+}
+
+export default configureStore();
